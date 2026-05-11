4c7e5194a889d43c5bb404a9738b8084341d8d1e
subhchitra143-tech <your-email-used-in-github>
2026-05-11 22:26:12 +0530

    fix production port

--- server/server.js
+++ server/server.js
@@ -8,16 +8,24 @@ const capsuleRoutes = require("./routes/capsuleRoutes");
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/capsule", capsuleRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
+
     console.log("MongoDB Connected");
-    app.listen(5000, () => {
-      console.log("Server Running on Port 5000");
+
+    app.listen(PORT, () => {
+      console.log(
+        `Server Running on Port ${PORT}`
+      );
     });
+
   })
   .catch((err) => console.log(err));
