2dc07cef469cbfe38dd720c3349acb06000a4f62
subhchitra143-tech <your-email-used-in-github>
2026-05-11 22:07:02 +0530

    fix production start

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
     try {
 
       const res = await axios.post(
-        "http://localhost:5000/api/capsule/create",
+        "https://couple-capsule.onrender.com",
         {
           personOne,
           personTwo,
